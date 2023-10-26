53ae7ec108a3ae2c687f4e2f5224841bda181f4d
Gabe261 <gabed.siewert@hotmail.com>
2023-10-26 17:57:53 -0400

    First Commit

---
+++ CodeJammin/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    [SerializeField] private int chickens = 0; 
+
+    [SerializeField] private TextMeshProUGUI bananasText;
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Banana"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            chickens++;
+            Debug.Log($"{chickens}");
+            bananasText.text = "Chickens: " + chickens;
+        }
+    }
+}
